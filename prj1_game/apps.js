53f2f4c1f0a907ddab7d395804d78ed1af7f4132
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-23 12:09:45 -0700

    Tracking spawn now functional, and they're terrifying especially when swarming in large numbers

--- prj1_game/apps.js
+++ prj1_game/apps.js
@@ -15,22 +15,24 @@ class Player {
         this.health = 10
         this.speed = 3
         this.color = 'rgb(255,0,0)'
-        this.x = boardOrigin.x
-        this.y = boardOrigin.y
+        this.x = 0
+        this.y = 0
+        //this.x = boardOrigin.x
+        //this.y = boardOrigin.y
         this.moveSpeed = .7
         this.radius = gameScaler * .01
         this.pathHistory = []
         this.jumpBackDist = 350
     }
 
-    movement(){
+    movement() {
 
         //nextMove global variable stores X or Y axis and 1 or -1 by arrowkeys. Ex: down arrowkey = ['y', 1] for increase y axis to move player down.
         let moveAxis = nextMove[0]
-        let moveDist = nextMove[1]*nextMove[2]*this.moveSpeed
+        let moveDist = nextMove[1] * nextMove[2] * this.moveSpeed
         moveAxis === 'x' ? (this.x + moveDist >= 0 && this.x + moveDist <= myCanvas.width ? this.x += moveDist : null) : null
         moveAxis === 'y' ? (this.y + moveDist >= 0 && this.y + moveDist <= myCanvas.height ? this.y += moveDist : null) : null
-        this.pathHistory.unshift([this.x,this.y])
+        this.pathHistory.unshift([this.x, this.y])
         this.pathHistory.length > this.jumpBackDist ? this.pathHistory.pop() : null
     }
 
@@ -38,24 +40,24 @@ class Player {
     render() {
         //console.log(this.pathHistory.length)
         //path history shadow, drawn first so player will overlay if on top.
-        if(this.pathHistory.length > this.jumpBackDist/2){
+        if (this.pathHistory.length > this.jumpBackDist / 2) {
             //console.log('draw history')
             pencil.beginPath()
             this.jumpBackDist === this.pathHistory.length ? pencil.strokeStyle = 'darkgreen' : pencil.strokeStyle = 'blue'
-            pencil.arc(this.pathHistory[this.pathHistory.length-1][0], this.pathHistory[this.pathHistory.length-1][1], this.radius, 0, 2 * Math.PI)
+            pencil.arc(this.pathHistory[this.pathHistory.length - 1][0], this.pathHistory[this.pathHistory.length - 1][1], this.radius, 0, 2 * Math.PI)
             pencil.fill()
 
             //draw the pathHistory outline
             let prevNode = [this.x, this.y]
-            for (let node of this.pathHistory){
-                pencil.moveTo(prevNode[0],prevNode[1])
+            for (let node of this.pathHistory) {
+                pencil.moveTo(prevNode[0], prevNode[1])
                 this.jumpBackDist === this.pathHistory.length ? pencil.fillStyle = 'darkgreen' : pencil.fillStyle = 'blue'
-                pencil.lineTo(node[0],node[1])
+                pencil.lineTo(node[0], node[1])
                 pencil.stroke()
                 prevNode = node
             }
         }
-        
+
         //Central player
         pencil.beginPath()
         pencil.fillStyle = this.color
@@ -63,13 +65,13 @@ class Player {
         pencil.fill()
     }
 
-    jumpBack(){
+    jumpBack() {
         //pick the time distance in seconds back, adjust for game tick/second, and revert state.
-        if(this.pathHistory.length === this.jumpBackDist){
-            let historicState = this.pathHistory[this.jumpBackDist-1]
+        if (this.pathHistory.length === this.jumpBackDist) {
+            let historicState = this.pathHistory[this.jumpBackDist - 1]
             //console.log(this.pathHistory)
             //console.log(historicState)
-            nextMove = [0,0,1]
+            nextMove = [0, 0, 1]
             this.x = historicState[0]
             this.y = historicState[1]
             this.pathHistory = []
@@ -87,18 +89,10 @@ class Spawn {
         this.dY = dY
         this.x = 0
         this.y = myCanvas.height / 2
-        this.color = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+        this.color = 'black'
         //console.log('My dX, dY is ' , this.dX,this.dY)
     }
-    movement() {
-        //console.log(this.x,this.dX,this.y,this.dY);
-        //check x coordinates for next move. If outside of board boundry, flip the sign of dX to make it go the other way next tick.
-        (this.x + this.dX + this.radius) <= myCanvas.width && (this.x + this.dX) >= 0 ? null : this.dX *= (-1)
-        this.x += this.dX;
-        //check y coords
-        (this.y + this.dY + this.radius) <= myCanvas.height && (this.y + this.dY) >= 0 ? null : this.dY *= (-1)
-        this.y += this.dY;
-    }
+
     render() {
         pencil.fillStyle = this.color
         pencil.fillRect(this.x, this.y, this.radius, this.radius)
@@ -108,15 +102,62 @@ class Spawn {
     }
 }
 
+class GeneralSpawn extends Spawn {
+    constructor(radius, dX, dY) {
+        super(radius, dX, dY)
+        //this.color=`rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+    }
+    movement() {
+        //Bounce off the walls and move across screen
+        (this.x + this.dX + this.radius) <= myCanvas.width && (this.x + this.dX) >= 0 ? null : this.dX *= (-1)
+        this.x += this.dX;
+        //check y coords
+        (this.y + this.dY + this.radius) <= myCanvas.height && (this.y + this.dY) >= 0 ? null : this.dY *= (-1)
+        this.y += this.dY;
+    }
+}
+
+class TrackingSpawn extends Spawn {
+    constructor(radius, dX, dY, speed) {
+        super(radius, dX, dY)
+        this.speed = speed
+    }
+    movement() {
+        //track the player and follow them after each collision with a wall
+        let trackX = player.x - this.x;
+        let trackY = player.y - this.y;
+        let netTrackDist = Math.sqrt((trackX ** 2) + (trackY ** 2));
+        let netTrackTime = netTrackDist; // (tot dist)/(tot time) === (base dist)/(gametick)
+        let trackDX = trackX / netTrackTime;
+        let trackDY = trackY / netTrackTime;
+        if ((this.x + this.dX + this.radius) <= myCanvas.width && (this.x + this.dX) >= 0) {
+        } else {
+            console.log('player and this x,y', player.x, player.y, this.x, this.y, 'track then D', trackX, trackY, trackDX, trackDY)
+            this.dX = trackDX
+            this.dY = trackDY
+        }
+        //check y coords
+        if((this.y + this.dY + this.radius) <= myCanvas.height && (this.y + this.dY) >= 0 ){ 
+        }else{
+            console.log('player and this x,y', player.x, player.y, this.x, this.y, 'track then D', trackX, trackY, trackDX, trackDY)
+            this.dY = trackDY
+            this.dX = trackDX
+        }
+        this.y += this.dY;
+        this.x += this.dX;
+    }
+
+}
+
 //generate new spawn dictated by rate, how much time has passed.
-function generateSpawn(rate, spawnSize, speedScaler) {
+function generateSpawn(rate, spawnSize, speedScaler, type) {
     //console.log(spawnTimer)
     if (spawnTimer > rate) {
         spawnTimer = 0
         let size = 10 + Math.floor(Math.random() * spawnSize)
-        let speedX = (speedScaler*0.5)-Math.random()*speedScaler
-        let speedY = (speedScaler*0.5)-Math.random()*speedScaler
-        let newSpawn = new Spawn(size, speedX, speedY)
+        let speedX = (speedScaler * 0.5) - Math.random() * speedScaler
+        let speedY = (speedScaler * 0.5) - Math.random() * speedScaler
+        let newSpawn = new type(size, speedX, speedY)
         spawnList.push(newSpawn)
     } else {
         spawnTimer++
@@ -125,13 +166,12 @@ function generateSpawn(rate, spawnSize, speedScaler) {
 
 //function to see if the player's been hit by one of the obstacles
 function checkCollisions() {
-    let playerCoords = [player.x, player.y]
 
-    for(let spawn of spawnList){
+    for (let spawn of spawnList) {
         //spawn hit box dimensions include player dimensions so that only player x and y coordinates need to be calculated after.
-        let spawnHitBox = {xMin:spawn.x-player.radius, xMax:spawn.x + spawn.radius + player.radius, yMin:spawn.y-player.radius, yMax:spawn.y+spawn.radius +player.radius}
+        let spawnHitBox = { xMin: spawn.x - player.radius, xMax: spawn.x + spawn.radius + player.radius, yMin: spawn.y - player.radius, yMax: spawn.y + spawn.radius + player.radius }
         //console.log(spawnHitBox)
-        if (player.x < spawnHitBox.xMax && player.x > spawnHitBox.xMin && player.y > spawnHitBox.yMin && player.y < spawnHitBox.yMax){
+        if (player.x < spawnHitBox.xMax && player.x > spawnHitBox.xMin && player.y > spawnHitBox.yMin && player.y < spawnHitBox.yMax) {
             console.log('Hit!')
             clearInterval(gameInterval)
         }
@@ -139,7 +179,7 @@ function checkCollisions() {
 }
 
 //function to reset the game
-function resetGame(){
+function resetGame() {
     clearInterval(gameInterval)
     spawnList = []
     score = 0
@@ -148,20 +188,22 @@ function resetGame(){
     player.x = boardOrigin.x
     player.y = boardOrigin.y
     gameInterval = setInterval(gameTick, 20)
-    
+
 }
 
 //function to update scoreboard and track current score
 let score = 0
 let scoreBoard = document.getElementById('scoreboard')
-function updateScoreboard (){
-    score+= spawnList.length/50
+function updateScoreboard() {
+    score += spawnList.length / 50
     scoreBoard.textContent = `Score: ${Math.floor(score)}`
 }
 
 //Function to advance the game by one 'tick' each 60ms.
 let spawnTimer = 0
-function gameTick()  {
+//let tSpawn = new TrackingSpawn(10, 0.5, 0.5, .5)
+//spawnList.push(tSpawn)
+function gameTick(gameMode) {
     //clear board and redraw
     pencil.clearRect(0, 0, myCanvas.width, myCanvas.height)
     player.movement()
@@ -170,15 +212,16 @@ function gameTick()  {
     updateScoreboard()
 
     //spawn stuff
-    generateSpawn(50, 5 , 0.5)
-    for(let spawn of spawnList){
+    generateSpawn(50, .5 , 0.5, TrackingSpawn)
+
+    for (let spawn of spawnList) {
         spawn.movement()
         spawn.render()
     }
 }
 
 //store the last arrowkey press in the nextMove array. Use that for movement direction. Spacebar to clear. format [axis, direction, speed]
-let nextMove = [0, 0 , 1]
+let nextMove = [0, 0, 1]
 //function to handle movement. Data is pushed by Player class. Can handle other keypressse
 function movementHandlerKeyDown(e) {
     //console.log('keyboard move was ', e.key)
@@ -200,13 +243,13 @@ function movementHandlerKeyDown(e) {
             nextMove[1] = 1
             break
         case " ":
-            nextMove = [0, 0 , 1]
+            nextMove = [0, 0, 1]
             break
         case "f":
-            player.jumpBack(3)  
+            player.jumpBack(3)
             break
         case 'Shift':
-            nextMove[2]=0.3
+            nextMove[2] = 0.3
             break
         case 'r':
             resetGame()
@@ -215,17 +258,17 @@ function movementHandlerKeyDown(e) {
 }
 
 //modify movement based on held keys
-function movementHandlerKeyUp(e){
+function movementHandlerKeyUp(e) {
     //console.log(e.key)
-    switch (e.key){
+    switch (e.key) {
         case 'Shift':
-            nextMove[2]=1
+            nextMove[2] = 1
     }
 }
 
 //event listeners for keyboard presses
-document.addEventListener( 'keydown', movementHandlerKeyDown)
-document.addEventListener( 'keyup' , movementHandlerKeyUp)
+document.addEventListener('keydown', movementHandlerKeyDown)
+document.addEventListener('keyup', movementHandlerKeyUp)
 
 //resetGame also starts the game by setting the interval.
 
